1edbd9f0a684f6d1841ab16fab89bfd4e0df0a35
tdotux <105792562+tdotux@users.noreply.github.com>
2022-05-24 21:34:50 -0300

    Update pi-script.sh

--- pi-script.sh
+++ pi-script.sh
@@ -57,8 +57,6 @@ echo -e "$(tput sgr0)\n\n"
 
 echo -e "127.0.0.1 localhost.localdomain localhost\n::1 localhost.localdomain localhost\n127.0.1.1 $HOST.localdomain $HOST" | sudo tee /etc/hosts
 
-echo -e "$(tput sgr0)\n\n"
-
 
 
 ###SENHA ROOT
