f7c51b6b6187520ec1ab0ee52145c6bbf5d4d249
tdotux <105792562+tdotux@users.noreply.github.com>
2022-06-03 17:35:35 -0300

    Update epi-script.sh

--- epi-script.sh
+++ epi-script.sh
@@ -127,20 +127,6 @@ cp /etc/sudoers /etc/sudoers.bak && sed -i '82c\ %wheel ALL=(ALL:ALL) ALL' /etc/
 
 
 
-###GRUB
-
-PASTA_EFI=/sys/firmware/efi
-if [ ! -d "$PASTA_EFI" ];then
-echo -e "Sistema Legacy"
-grub-install --target=i386-pc /dev/sda --force && grub-mkconfig -o /boot/grub/grub.cfg
-
-else
-echo -e "Sistema EFI"
-pacman -S efibootmgr --noconfirm
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Arch --removable && grub-mkconfig -o /boot/grub/grub.cfg
-
-fi
-
 
 
 ###DRIVER DE VIDEO
@@ -319,172 +305,13 @@ echo -e "$(tput sgr0)\n\n"
 
 
 
-###PACOTES PADRÃO
-
-pacman -S xorg-server xorg-xinit xterm linux-zen-headers networkmanager tar gzip bzip2 zip unzip unrar p7zip pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber xdg-user-dirs gnome-disk-utility neofetch --noconfirm
-
-
-
-###INTERFACE GRÁFICA
-
-echo -e "$(tput bel)$(tput bold)$(tput setaf 7)$(tput setab 4)"
-
-echo -e "#### INTERFACE GRAFICA (DE) ####"
-
-echo -e "\n"
-
-echo -e "1 - Budgie"
-
-echo -e "2 - Cinnamon"
-
-echo -e "3 - Deepin"
-
-echo -e "4 - GNOME"
-
-echo -e "5 - KDE Plasma (X11)"
-
-echo -e "6 - KDE Plasma (Wayland)"
-
-echo -e "7 - LXDE"
-
-echo -e "8 - LXQt"
-
-echo -e "9 - MATE"
-
-echo -e "0 - XFCE"
-
-echo -e "\n"
-
-echo -ne "Escolha uma DE : "
-read -n1 -s DE
-
-case $DE in
-
-"1")
-
-echo "Budge"
-sleep 2
-echo -e "$(tput sgr0)\n\n"
-pacman -S budgie-desktop gnome-terminal gedit gnome-calculator gnome-calendar gnome-system-monitor nautilus network-manager-applet lightdm lightdm-gtk-greeter --noconfirm
-systemctl enable lightdm NetworkManager
-
-;;
-
-"2")
-
-echo "Cinnamon"
-
-sleep 2
-echo -e "$(tput sgr0)\n\n"
-pacman -S cinnamon network-manager-applet lightdm lightdm-gtk-greeter --noconfirm
-systemctl enable lightdm NetworkManager
-
-;;
-
-"3")
-
-echo "Deepin"
-
-sleep 2
-echo -e "$(tput sgr0)\n\n"
-pacman -S deepin network-manager-applet lightdm lightdm-gtk-greeter --noconfirm
-systemctl enable lightdm NetworkManager
-
-;;
-
-"4")
-
-echo "Gnome"
-
-sleep 2
-echo -e "$(tput sgr0)\n\n"
-pacman -S gnome gnome-tweaks network-manager-applet gdm --noconfirm
-systemctl enable gdm NetworkManager
-
-;;
-
-"5")
-
-echo "KDE Plasma (X11)"
-
-sleep 2
-echo -e "$(tput sgr0)\n\n"
-pacman -S plasma konsole sddm dolphin spectacle kcalc kwrite gwenview plasma-nm plasma-pa --noconfirm
-systemctl enable sddm NetworkManager
-
-;;
-
-"6")
-
-echo "KDE Plasma (Wayland)"
-
-sleep 2
-echo -e "$(tput sgr0)\n\n"
-pacman -S plasma konsole sddm dolphin spectacle kcalc kwrite gwenview plasma-nm plasma-pa plasma-wayland-session --noconfirm
-systemctl enable sddm NetworkManager
-
-;;
-
-"7")
-
-echo "LXDE"
-
-sleep 2
-echo -e "$(tput sgr0)\n\n"
-pacman -S lxde-gtk3 lxtask network-manager-applet lightdm lightdm-gtk-greeter --noconfirm
-systemctl enable lightdm NetworkManager
-
-;;
-
-"8")
-
-echo "LXQT"
-
-sleep 2
-echo -e "$(tput sgr0)\n\n"
-pacman -S lxqt lxtask network-manager-applet sddm --noconfirm
-systemctl enable sddm NetworkManager
-
-;;
-
-"9")
-
-echo "MATE"
-
-sleep 2
-echo -e "$(tput sgr0)\n\n"
-pacman -S mate mate-extra network-manager-applet lightdm lightdm-gtk-greeter --noconfirm
-systemctl enable lightdm NetworkManager
-
-;;
-
-"0")
-
-echo "XFCE"
-
-sleep 2
-echo -e "$(tput sgr0)\n\n"
-pacman -S xfce4 xfce4-screenshooter xfce4-pulseaudio-plugin xfce4-whiskermenu-plugin xarchiver lxtask ristretto mousepad galculator thunar-archive-plugin network-manager-applet lightdm lightdm-gtk-greeter --noconfirm
-systemctl enable lightdm NetworkManager
-
-esac
-
-
-echo -e "$(tput sgr0)\n\n"
-
-
-
-###USER DIRS UPDATE
-
-xdg-user-dirs-update
-
 
 
 ###SWAP FILE
 
 echo -e "$(tput bel)$(tput bold)$(tput setaf 7)$(tput setab 4)"
 
-echo -e "Swap"
+echo -e "### Swap ###"
 
 echo -e "\n"
 
@@ -574,6 +401,255 @@ fallocate -l 4G /swapfile && chmod 600 /swapfile && mkswap /swapfile && swapon /
 fi
 
 esac
+
+
+
+###INTERFACE GRÁFICA
+
+echo -e "$(tput bel)$(tput bold)$(tput setaf 7)$(tput setab 4)"
+
+echo -e "#### INTERFACE GRAFICA (DE) ####"
+
+echo -e "\n"
+
+echo -e "1 - Budgie"
+
+echo -e "2 - Cinnamon"
+
+echo -e "3 - Deepin"
+
+echo -e "4 - GNOME"
+
+echo -e "5 - KDE Plasma (X11)"
+
+echo -e "6 - KDE Plasma (Wayland)"
+
+echo -e "7 - LXDE"
+
+echo -e "8 - LXQt"
+
+echo -e "9 - MATE"
+
+echo -e "0 - XFCE"
+
+echo -e "\n"
+
+echo -ne "Escolha uma DE : "
+read -n1 -s DE
+
+case $DE in
+
+"1")
+
+echo "Budge"
+sleep 2
+echo -e "$(tput sgr0)\n\n"
+
+##Pacotes Padrão
+
+pacman -S xorg-server xorg-xinit xterm networkmanager tar gzip bzip2 zip unzip unrar p7zip pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber xdg-user-dirs gnome-disk-utility neofetch --noconfirm
+
+##Interface e DM
+
+pacman -S budgie-desktop gnome-terminal gedit gnome-calculator gnome-calendar gnome-system-monitor nautilus network-manager-applet lightdm lightdm-gtk-greeter --noconfirm
+systemctl enable lightdm NetworkManager
+
+;;
+
+"2")
+
+echo "Cinnamon"
+
+sleep 2
+echo -e "$(tput sgr0)\n\n"
+
+##Pacotes Padrão
+
+pacman -S xorg-server xorg-xinit xterm networkmanager tar gzip bzip2 zip unzip unrar p7zip pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber xdg-user-dirs gnome-disk-utility neofetch --noconfirm
+
+##Interface e DM
+
+pacman -S cinnamon network-manager-applet lightdm lightdm-gtk-greeter --noconfirm
+systemctl enable lightdm NetworkManager
+
+;;
+
+"3")
+
+echo "Deepin"
+
+sleep 2
+echo -e "$(tput sgr0)\n\n"
+
+##Pacotes Padrão
+
+pacman -S xorg-server xorg-xinit xterm networkmanager tar gzip bzip2 zip unzip unrar p7zip pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber xdg-user-dirs gnome-disk-utility neofetch --noconfirm
+
+##Interface e DM
+
+pacman -S deepin network-manager-applet lightdm lightdm-gtk-greeter --noconfirm
+systemctl enable lightdm NetworkManager
+
+;;
+
+"4")
+
+echo "Gnome"
+
+sleep 2
+echo -e "$(tput sgr0)\n\n"
+
+##Pacotes Padrão
+
+pacman -S xorg-server xorg-xinit xterm networkmanager tar gzip bzip2 zip unzip unrar p7zip pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber xdg-user-dirs gnome-disk-utility neofetch --noconfirm
+
+##Interface e DM
+
+pacman -S gnome gnome-tweaks network-manager-applet gdm --noconfirm
+systemctl enable gdm NetworkManager
+
+;;
+
+"5")
+
+echo "KDE Plasma (X11)"
+
+sleep 2
+echo -e "$(tput sgr0)\n\n"
+
+##Pacotes Padrão
+
+pacman -S xorg-server xorg-xinit xterm networkmanager tar gzip bzip2 zip unzip unrar p7zip pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber xdg-user-dirs gnome-disk-utility neofetch --noconfirm
+
+##Interface e DM
+
+pacman -S plasma konsole sddm dolphin spectacle kcalc kwrite gwenview plasma-nm plasma-pa --noconfirm
+systemctl enable sddm NetworkManager
+
+;;
+
+"6")
+
+echo "KDE Plasma (Wayland)"
+
+sleep 2
+echo -e "$(tput sgr0)\n\n"
+
+##Pacotes Padrão
+
+pacman -S xorg-server xorg-xinit xterm networkmanager tar gzip bzip2 zip unzip unrar p7zip pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber xdg-user-dirs gnome-disk-utility neofetch --noconfirm
+
+##Interface e DM
+
+pacman -S plasma konsole sddm dolphin spectacle kcalc kwrite gwenview plasma-nm plasma-pa plasma-wayland-session --noconfirm
+systemctl enable sddm NetworkManager
+
+;;
+
+"7")
+
+echo "LXDE"
+
+sleep 2
+echo -e "$(tput sgr0)\n\n"
+
+##Pacotes Padrão
+
+pacman -S xorg-server xorg-xinit xterm networkmanager tar gzip bzip2 zip unzip unrar p7zip pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber xdg-user-dirs gnome-disk-utility neofetch --noconfirm
+
+##Interface e DM
+
+pacman -S lxde-gtk3 lxtask network-manager-applet lightdm lightdm-gtk-greeter --noconfirm
+systemctl enable lightdm NetworkManager
+
+;;
+
+"8")
+
+echo "LXQT"
+
+sleep 2
+echo -e "$(tput sgr0)\n\n"
+
+##Pacotes Padrão
+
+pacman -S xorg-server xorg-xinit xterm networkmanager tar gzip bzip2 zip unzip unrar p7zip pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber xdg-user-dirs gnome-disk-utility neofetch --noconfirm
+
+##Interface e DM
+
+pacman -S lxqt lxtask network-manager-applet sddm --noconfirm
+systemctl enable sddm NetworkManager
+
+;;
+
+"9")
+
+echo "MATE"
+
+sleep 2
+echo -e "$(tput sgr0)\n\n"
+
+##Pacotes Padrão
+
+pacman -S xorg-server xorg-xinit xterm networkmanager tar gzip bzip2 zip unzip unrar p7zip pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber xdg-user-dirs gnome-disk-utility neofetch --noconfirm
+
+##Interface e DM
+
+pacman -S mate mate-extra network-manager-applet lightdm lightdm-gtk-greeter --noconfirm
+systemctl enable lightdm NetworkManager
+
+;;
+
+"0")
+
+echo "XFCE"
+
+sleep 2
+echo -e "$(tput sgr0)\n\n"
+
+##Pacotes Padrão
+
+pacman -S xorg-server xorg-xinit xterm networkmanager tar gzip bzip2 zip unzip unrar p7zip pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber xdg-user-dirs gnome-disk-utility neofetch --noconfirm
+
+##Interface e DM
+
+pacman -S xfce4 xfce4-screenshooter xfce4-pulseaudio-plugin xfce4-whiskermenu-plugin xarchiver lxtask ristretto mousepad galculator thunar-archive-plugin network-manager-applet lightdm lightdm-gtk-greeter --noconfirm
+systemctl enable lightdm NetworkManager
+
+esac
+
+
+echo -e "$(tput sgr0)\n\n"
+
+
+
+
+
+###USER DIRS UPDATE
+
+xdg-user-dirs-update
+
+
+
+
+
+###GRUB
+
+PASTA_EFI=/sys/firmware/efi
+if [ ! -d "$PASTA_EFI" ];then
+echo -e "Sistema Legacy"
+grub-install --target=i386-pc /dev/sda --force && grub-mkconfig -o /boot/grub/grub.cfg
+
+else
+echo -e "Sistema EFI"
+pacman -S efibootmgr --noconfirm
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Arch --removable && grub-mkconfig -o /boot/grub/grub.cfg
+
+fi
+
+
+
+
 
 echo -e "$(tput bel)$(tput bold)$(tput setaf 7)$(tput setab 4)"
 
